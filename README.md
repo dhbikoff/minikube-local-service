24d18da4d9f74b9c84b85709d846284c9c8caa5f
Derek Bikoff <dhbikoff@gmail.com>
2019-11-15 20:54:02 -0500

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# Local Service Helm Chart
+
+A simple helm chart that will put a dev service, running on your host, behind a minikube ingress route.
+
+`ex: myhost.dev/myservice/foo`
+
+## Deploying the Helm chart
+Your host service must be bound to 0.0.0.0, so that it's accessible from within the minikube vm.
+
+The following script example will deploy a helm chart into k8s that wires up a local service, running on port 3000,
+and makes it accessible at myhost.dev/myservice/*
+
+`$ ./deploy.sh -n myservice -h myhost.dev -p 3000`
+
+You can hit your endpoints. Note this curl example uses the `-L` flag, which follows redirects.
+The k8s Nginx ingress controller redirects requests to your host service.
+
+`$ curl -L myhost.dev/myservice/ping`
+
+You can attach as many services as you like, just use a different `path-part` and `host-port` for each.
+
+`$ ./deploy.sh -n myservice -h myhost.dev -p 3000`
+`$ ./deploy.sh -n myservice-ui -h myhost.dev -p 3001`
+
+## Deleting a local service
+
+`$ ./deploy.sh -d myservice`
---
+++ deploy.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+function USAGE {
+  echo "Basic usage: ./deploy.sh -n myservice -h myhost.dev"
+  echo "Flags:"
+  echo "-h Ingress host"
+  echo "-n Name for your helm package and url path part behind your ingress host - Default: \"default\""
+	echo "-p Target service host port - Default: 3000"
+	echo "-d Delete previously created helm package name, ex. ./deploy.sh -d default"
+  echo "Example: ./deploy.sh -n myservice -h myhost.dev -p 3001"
+  exit 1
+}
+
+PACKAGE_SUFFIX="-localservice"
+NAME="default"
+PORT=3000
+INGRESS_HOST=
+
+while getopts :d:h:n:p FLAG; do
+  case $FLAG in
+    d)
+      DEL_PACKAGE="$OPTARG"
+      ;;
+    h)
+      INGRESS_HOST="$OPTARG"
+      ;;
+    n)
+      NAME="$OPTARG"
+      ;;
+    p)
+      PORT="$OPTARG"
+      ;;
+    *)
+      USAGE
+      ;;
+  esac
+done
+
+if [ ! -z $DEL_PACKAGE ]; then
+  echo "Removing helm package ${DEL_PACKAGE}${PACKAGE_SUFFIX}"
+  helm del --purge "${DEL_PACKAGE}${PACKAGE_SUFFIX}"
+  exit $?
+fi
+
+if [ -z $INGRESS_HOST ]; then
+  echo "Missing ingress host"
+  USAGE
+fi
+
+VM_TO_HOST_IP=$(minikube ssh -- route -n | grep ^0.0.0.0 | awk '{ print $2 }' | xargs echo -n)
+echo found host ip from within vm $VM_TO_HOST_IP
+MINIKUBE_IP=$(minikube ip | xargs echo -n)
+echo found minikube ip $MINIKUBE_IP
+
+helm install --debug ./localservice -n "${NAME}${PACKAGE_SUFFIX}" \
+  --set targetPort=$PORT \
+  --set minikubeIp=$MINIKUBE_IP \
+  --set pathPart=$NAME \
+  --set vmToHostIp=$VM_TO_HOST_IP \
+  --set ingressHost=$INGRESS_HOST
